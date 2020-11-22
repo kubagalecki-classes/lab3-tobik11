151d60df18851217f2974896ed0491e293db822a
tobik11 <tobik11@onet.pl>
2020-11-22 18:42:25 +0000

    first try

--- lab3.cpp
+++ lab3.cpp
@@ -1,5 +1,41 @@
 #include <iostream>
 
+class Statek {
+public:
+	virtual unsigned int transportuj() {
+
+	}
+
+};
+
+
+class Tankowiec : public Statek {
+public:
+	unsigned int transportuj() override{
+
+	}
+};
+
+class Zaglowiec : public Statek {
+public:
+	unsigned int transportuj() override {
+
+	}
+};
+
+class Stocznia {
+	Statek* operator()() {
+		int p = rand() % 100;
+		if (p > 50)
+			return new Tankowiec{};
+		else
+			return new Zaglowiec{};
+	}
+};
+
+
+
+
 int main()
 {
     puts("Hello, World!");
--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -4,6 +4,32 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+   	if (towar == 0)
+		return 0;
+
+	unsigned int transported = 0;
+	unsigned int zaglowce = 0;
+	Stocznia st;
+
+	while (1) {
+		Statek* s = st();
+		transported += (s->transportuj());
+		cout << transported << endl;
+		// check if zaglowiec
+		Statek* ptr;
+		ptr = dynamic_cast <Zaglowiec*>(s);
+		if (ptr != nullptr)
+			zaglowce++;
+		cout << ptr << endl;
+
+		delete s;
+
+		if (transported >= towar)
+			break;
+	}
+
+
+
+	cout << zaglowce << endl;
+	return zaglowce;
 }
